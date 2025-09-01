b70dadab231638604725edabc4c1903e2905b4ac
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2025-09-01 16:21:16 +0500

    form validation started

--- lib/views/components/design_card.dart
+++ lib/views/components/design_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smile_ai/models/design.dart';
+import 'package:smile_ai/views/pages/design_detail.dart';
 
 class DesignCard extends StatelessWidget {
   final Design design;
@@ -8,38 +9,45 @@ class DesignCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: colorScheme.secondary.withOpacity(0.05),
-      ),
-      padding: const EdgeInsets.all(12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: colorScheme.secondary.withOpacity(0.2),
-                image: DecorationImage(
-                  image: AssetImage(design.imageUrl),
-                  fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(
+          context,
+        ).push(MaterialPageRoute(builder: (context) => DesignPage()));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: colorScheme.secondary.withOpacity(0.05),
+        ),
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: colorScheme.secondary.withOpacity(0.2),
+                  image: DecorationImage(
+                    image: AssetImage(design.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
                 ),
+                // child: Image.asset(design.imageUrl),
+                // child: const Center(child: Icon(Icons.image, size: 48)),
               ),
-              // child: Image.asset(design.imageUrl),
-              // child: const Center(child: Icon(Icons.image, size: 48)),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            design.name,
-            style: TextStyle(
-              color: colorScheme.onBackground,
-              fontWeight: FontWeight.w600,
+            const SizedBox(height: 8),
+            Text(
+              design.name,
+              style: TextStyle(
+                color: colorScheme.onBackground,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/views/pages/login.dart
+++ lib/views/pages/login.dart
@@ -3,185 +3,205 @@ import 'package:smile_ai/views/pages/email_verification.dart';
 import 'package:smile_ai/views/pages/register.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+
+  TextEditingController _passwordController = TextEditingController();
+
+  void submitLoginForm() {
+    print("form is being submitted ...");
+
+    // submit the form
+    if (_formKey.currentState!.validate()) {
+      print("form is submitted");
+      print("Email: ${_emailController.text}");
+
+      print("password: ${_passwordController.text}");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
-
     return Scaffold(
       backgroundColor: colors.background,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Title
-              Text(
-                "Welcome Back 👋",
-                style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  color: colors.onBackground,
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "Login to continue creating stunning AI photos",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: colors.secondary,
-                ),
-              ),
-
-              const SizedBox(height: 40),
-
-              // Email Field
-              TextFormField(
-                style: TextStyle(color: colors.onSurface),
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  labelStyle: TextStyle(color: colors.secondary),
-                  filled: true,
-                  fillColor: colors.surface,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: colors.secondary.withOpacity(0.3)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: colors.primary, width: 2),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Title
+                Text(
+                  "Welcome Back 👋",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: colors.onBackground,
                   ),
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-
-              const SizedBox(height: 20),
-
-              // Password Field
-              TextFormField(
-                style: TextStyle(color: colors.onSurface),
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  labelStyle: TextStyle(color: colors.secondary),
-                  filled: true,
-                  fillColor: colors.surface,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: colors.secondary.withOpacity(0.3)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: colors.primary, width: 2),
-                  ),
+                const SizedBox(height: 8),
+                Text(
+                  "Login to continue creating stunning AI photos",
+                  style: TextStyle(fontSize: 14, color: colors.secondary),
                 ),
-                obscureText: true,
-              ),
 
-              const SizedBox(height: 12),
+                const SizedBox(height: 40),
 
-              // Forgot Password
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Forgot password?",
-                    style: TextStyle(color: colors.primary),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 30),
-
-              // Login Button
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                     // Navigate to login  screen
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => EmailVerificationScreen(email: "shahzadfarooqdev@gmail.com",)),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: colors.primary,
-                    foregroundColor: colors.onPrimary,
-                    shape: RoundedRectangleBorder(
+                // Email Field
+                TextFormField(
+                  controller: _emailController,
+                  style: TextStyle(color: colors.onSurface),
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    labelStyle: TextStyle(color: colors.secondary),
+                    filled: true,
+                    fillColor: colors.surface,
+                    enabledBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                        color: colors.secondary.withOpacity(0.3),
+                      ),
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                  ),
-                  child: const Text("Login"),
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              // Divider with OR
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(color: colors.secondary.withOpacity(0.3)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                    child: Text(
-                      "or",
-                      style: TextStyle(color: colors.secondary),
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(color: colors.secondary.withOpacity(0.3)),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 20),
-
-              // Social Login Button
-              SizedBox(
-                width: double.infinity,
-                child: OutlinedButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    side: BorderSide(color: colors.secondary.withOpacity(0.4)),
-                    foregroundColor: colors.onBackground,
-                    shape: RoundedRectangleBorder(
+                    focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: colors.primary, width: 2),
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 14),
                   ),
-                  child: const Text("Continue with Google"),
+                  keyboardType: TextInputType.emailAddress,
                 ),
-              ),
 
-              const Spacer(),
+                const SizedBox(height: 20),
 
-              // Sign Up Redirect
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don’t have an account?",
-                    style: TextStyle(color: colors.secondary),
+                // Password Field
+                TextFormField(
+                  controller: _passwordController,
+                  style: TextStyle(color: colors.onSurface),
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: colors.secondary),
+                    filled: true,
+                    fillColor: colors.surface,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(
+                        color: colors.secondary.withOpacity(0.3),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: colors.primary, width: 2),
+                    ),
                   ),
-                  TextButton(
-                    onPressed: () {
-                       // Navigate to login  screen
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => RegisterScreen()),
-                    );
-                    },
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 12),
+
+                // Forgot Password
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
                     child: Text(
-                      "Sign Up",
+                      "Forgot password?",
                       style: TextStyle(color: colors.primary),
                     ),
-                  )
-                ],
-              ),
-            ],
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                // Login Button
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: submitLoginForm,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: colors.primary,
+                      foregroundColor: colors.onPrimary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                    ),
+                    child: const Text("Login"),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                // Divider with OR
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(color: colors.secondary.withOpacity(0.3)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: Text(
+                        "or",
+                        style: TextStyle(color: colors.secondary),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(color: colors.secondary.withOpacity(0.3)),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+
+                // Social Login Button
+                SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: colors.secondary.withOpacity(0.4),
+                      ),
+                      foregroundColor: colors.onBackground,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 14),
+                    ),
+                    child: const Text("Continue with Google"),
+                  ),
+                ),
+
+                const Spacer(),
+
+                // Sign Up Redirect
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don’t have an account?",
+                      style: TextStyle(color: colors.secondary),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Navigate to login  screen
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => RegisterScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(color: colors.primary),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
